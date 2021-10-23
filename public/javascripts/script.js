b9fb8d4205ba2807e870d663e57b4d72e49a8f21
Jithin S <jithins41@gmail.com>
2021-10-23 20:40:18 +0530

    Registration under processing

---
+++ public/javascripts/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  setTimeout( function () {
+    $("#loader").hide();
+  },1000    );
+});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.redirect('/login')
+router.get("/", function (req, res, next) {
+  res.redirect("/login");
 });
-router.get('/login', function (req, res, next) {
-  res.render('login', { title: 'Express' });
-})
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Express" });
+});
+router.get("/register", function (req, res, next) {
+  res.render("employees/register-employee");
+});
 module.exports = router;
